10e8b43d72108e030b9e72533a1d8faf5903ccc9
Doug Martin <dmartin@concord.org>
2021-11-18 07:46:18 -0500

    Added start of component and uploader page

---
+++ src/component/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./index.scss";
+
+export const ImageUploader = () => {
+  return (
+    <div className="uploader-component">
+      TODO: Image Uploader Component
+    </div>
+  );
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,127 +4,251 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const ESLintPlugin = require('eslint-webpack-plugin');
 const os = require('os');
 
 module.exports = (env, argv) => {
   const devMode = argv.mode !== 'production';
 
-  return {
-    context: __dirname, // to automatically find tsconfig.json
-    devServer: {
-      static: 'dist',
-      hot: true,
-      https: {
-        key: path.resolve(os.homedir(), '.localhost-ssl/localhost.key'),
-        cert: path.resolve(os.homedir(), '.localhost-ssl/localhost.pem'),
+  return [
+    // uploader page
+    {
+      context: __dirname, // to automatically find tsconfig.json
+      devServer: {
+        static: 'dist',
+        hot: true,
+        https: {
+          key: path.resolve(os.homedir(), '.localhost-ssl/localhost.key'),
+          cert: path.resolve(os.homedir(), '.localhost-ssl/localhost.pem'),
+        },
       },
-    },
-    devtool: devMode ? 'eval-cheap-module-source-map' : 'source-map',
-    entry: './src/index.tsx',
-    mode: 'development',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'assets/index.[contenthash].js',
-    },
-    performance: { hints: false },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader'
-        },
-        // This code coverage instrumentation should only be added when needed. It makes
-        // the code larger and slower
-        process.env.CODE_COVERAGE ? {
-          test: /\.[tj]sx?$/,
-          loader: 'istanbul-instrumenter-loader',
-          options: { esModules: true },
-          enforce: 'post',
-          exclude: path.join(__dirname, 'node_modules'),
-        } : {},
-        {
-          test: /\.(sa|sc|le|c)ss$/i,
-          use: [
-            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                esModule: false,
-                modules: {
-                  // required for :import from scss files
-                  // cf. https://github.com/webpack-contrib/css-loader#separating-interoperable-css-only-and-css-module-features
-                  mode: 'icss'
+      devtool: devMode ? 'eval-cheap-module-source-map' : 'source-map',
+      entry: './src/external-device/index.tsx',
+      mode: 'development',
+      output: {
+        path: path.join(path.resolve(__dirname, 'dist'), 'external-device'),
+        filename: 'assets/index.[contenthash].js',
+      },
+      performance: { hints: false },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader'
+          },
+          // This code coverage instrumentation should only be added when needed. It makes
+          // the code larger and slower
+          process.env.CODE_COVERAGE ? {
+            test: /\.[tj]sx?$/,
+            loader: 'istanbul-instrumenter-loader',
+            options: { esModules: true },
+            enforce: 'post',
+            exclude: path.join(__dirname, 'node_modules'),
+          } : {},
+          {
+            test: /\.(sa|sc|le|c)ss$/i,
+            use: [
+              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  esModule: false,
+                  modules: {
+                    // required for :import from scss files
+                    // cf. https://github.com/webpack-contrib/css-loader#separating-interoperable-css-only-and-css-module-features
+                    mode: 'icss'
+                  }
                 }
-              }
-            },
-            'postcss-loader',
-            'sass-loader'
-          ]
-        },
-        {
-          test: /\.(png|woff|woff2|eot|ttf)$/,
-          type: 'asset'
-        },
-        { // disable svgo optimization for files ending in .nosvgo.svg
-          test: /\.nosvgo\.svg$/i,
-          loader: '@svgr/webpack',
-          options: {
-            svgo: false
-          }
-        },
-        {
-          test: /\.svg$/i,
-          exclude: /\.nosvgo\.svg$/i,
-          oneOf: [
-            {
-              // Do not apply SVGR import in CSS files.
-              issuer: /\.(css|scss|less)$/,
-              type: 'asset'
-            },
-            {
-              issuer: /\.tsx?$/,
-              loader: '@svgr/webpack',
-              options: {
-                svgoConfig: {
-                  plugins: [
-                    // leave <line>s, <rect>s and <circle>s alone
-                    // https://github.com/svg/svgo/blob/master/plugins/convertShapeToPath.js
-                    { convertShapeToPath: false },
-                    // leave "class"es and "id"s alone
-                    // https://github.com/svg/svgo/blob/master/plugins/prefixIds.js
-                    { prefixIds: false },
-                    // leave "stroke"s and "fill"s alone
-                    // https://github.com/svg/svgo/blob/master/plugins/removeUnknownsAndDefaults.js
-                    { removeUnknownsAndDefaults: { defaultAttrs: false } }
-                  ]
-                }
-              }
+              },
+              'postcss-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.(png|woff|woff2|eot|ttf)$/,
+            type: 'asset'
+          },
+          { // disable svgo optimization for files ending in .nosvgo.svg
+            test: /\.nosvgo\.svg$/i,
+            loader: '@svgr/webpack',
+            options: {
+              svgo: false
             }
-          ]
-        }
+          },
+          {
+            test: /\.svg$/i,
+            exclude: /\.nosvgo\.svg$/i,
+            oneOf: [
+              {
+                // Do not apply SVGR import in CSS files.
+                issuer: /\.(css|scss|less)$/,
+                type: 'asset'
+              },
+              {
+                issuer: /\.tsx?$/,
+                loader: '@svgr/webpack',
+                options: {
+                  svgoConfig: {
+                    plugins: [
+                      // leave <line>s, <rect>s and <circle>s alone
+                      // https://github.com/svg/svgo/blob/master/plugins/convertShapeToPath.js
+                      { convertShapeToPath: false },
+                      // leave "class"es and "id"s alone
+                      // https://github.com/svg/svgo/blob/master/plugins/prefixIds.js
+                      { prefixIds: false },
+                      // leave "stroke"s and "fill"s alone
+                      // https://github.com/svg/svgo/blob/master/plugins/removeUnknownsAndDefaults.js
+                      { removeUnknownsAndDefaults: { defaultAttrs: false } }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      },
+      resolve: {
+        extensions: [ '.ts', '.tsx', '.js' ]
+      },
+      stats: {
+        // suppress "export not found" warnings about re-exported types
+        warningsFilter: /export .* was not found in/
+      },
+      plugins: [
+        new ESLintPlugin({
+          extensions: ['ts', 'tsx', 'js', 'jsx'],
+        }),
+        new MiniCssExtractPlugin({
+          filename: devMode ? "assets/[name].css" : "assets/[name].[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: 'src/external-device/index.html',
+          favicon: 'src/external-device/public/favicon.ico'
+        }),
+        new CleanWebpackPlugin(),
       ]
     },
-    resolve: {
-      extensions: [ '.ts', '.tsx', '.js' ]
+
+    // component
+    {
+      context: __dirname, // to automatically find tsconfig.json
+      devtool: devMode ? 'eval-cheap-module-source-map' : 'source-map',
+      entry: './src/component/index.tsx',
+      mode: 'development',
+      output: {
+        path: path.join(path.resolve(__dirname, 'dist'), 'component'),
+        filename: 'index.js',
+        library: 'ImageUploader',
+        libraryTarget: 'umd'
+      },
+      performance: { hints: false },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            options: {
+              configFile: "tsconfig-component.json"
+            }
+          },
+          // This code coverage instrumentation should only be added when needed. It makes
+          // the code larger and slower
+          process.env.CODE_COVERAGE ? {
+            test: /\.[tj]sx?$/,
+            loader: 'istanbul-instrumenter-loader',
+            options: { esModules: true },
+            enforce: 'post',
+            exclude: path.join(__dirname, 'node_modules'),
+          } : {},
+          {
+            test: /\.(sa|sc|le|c)ss$/i,
+            use: [
+              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  esModule: false,
+                  modules: {
+                    // required for :import from scss files
+                    // cf. https://github.com/webpack-contrib/css-loader#separating-interoperable-css-only-and-css-module-features
+                    mode: 'icss'
+                  }
+                }
+              },
+              'postcss-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.(png|woff|woff2|eot|ttf)$/,
+            type: 'asset'
+          },
+          { // disable svgo optimization for files ending in .nosvgo.svg
+            test: /\.nosvgo\.svg$/i,
+            loader: '@svgr/webpack',
+            options: {
+              svgo: false
+            }
+          },
+          {
+            test: /\.svg$/i,
+            exclude: /\.nosvgo\.svg$/i,
+            oneOf: [
+              {
+                // Do not apply SVGR import in CSS files.
+                issuer: /\.(css|scss|less)$/,
+                type: 'asset'
+              },
+              {
+                issuer: /\.tsx?$/,
+                loader: '@svgr/webpack',
+                options: {
+                  svgoConfig: {
+                    plugins: [
+                      // leave <line>s, <rect>s and <circle>s alone
+                      // https://github.com/svg/svgo/blob/master/plugins/convertShapeToPath.js
+                      { convertShapeToPath: false },
+                      // leave "class"es and "id"s alone
+                      // https://github.com/svg/svgo/blob/master/plugins/prefixIds.js
+                      { prefixIds: false },
+                      // leave "stroke"s and "fill"s alone
+                      // https://github.com/svg/svgo/blob/master/plugins/removeUnknownsAndDefaults.js
+                      { removeUnknownsAndDefaults: { defaultAttrs: false } }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      },
+      resolve: {
+        extensions: [ '.ts', '.tsx', '.js' ]
+      },
+      stats: {
+        // suppress "export not found" warnings about re-exported types
+        warningsFilter: /export .* was not found in/
+      },
+      plugins: [
+        new ESLintPlugin({
+          extensions: ['ts', 'tsx', 'js', 'jsx'],
+        }),
+        new MiniCssExtractPlugin({
+          filename: "assets/[name].css"
+        }),
+        new CopyPlugin({
+          patterns: [
+            { from: "src/component/package.json", to: "." }
+          ],
+        }),
+        new CleanWebpackPlugin(),
+      ],
+      externals: [
+        'react',
+        '@concord-consortium/lara-interactive-api'
+      ],
     },
-    stats: {
-      // suppress "export not found" warnings about re-exported types
-      warningsFilter: /export .* was not found in/
-    },
-    plugins: [
-      new ESLintPlugin({
-        extensions: ['ts', 'tsx', 'js', 'jsx'],
-      }),
-      new MiniCssExtractPlugin({
-        filename: devMode ? "assets/[name].css" : "assets/[name].[contenthash].css"
-      }),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'src/index.html',
-        favicon: 'src/public/favicon.ico'
-      }),
-      new CleanWebpackPlugin(),
-    ]
-  };
+  ]
 };
